a7a42a97682b429d860e83dcd3b0ebe4548bb55b
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-02-10 11:32:10 +0600

    easy

--- 3174. Clear Digits.py
+++ 3174. Clear Digits.py
@@ -1,2 +1,12 @@
 class Solution:
     def clearDigits(self, s: str) -> str:
+        stack = []
+        
+        for char in s:
+            if char.isdigit():
+                if stack:  # Remove the closest left letter
+                    stack.pop()
+            else:
+                stack.append(char)  # Push letters to stack
+
+        return "".join(stack)
