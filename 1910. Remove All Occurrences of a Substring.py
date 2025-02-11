69605a0dc55f4466dd5e33947521e837c6128552
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-02-11 21:03:12 +0600

    medium

--- 1910. Remove All Occurrences of a Substring.py
+++ 1910. Remove All Occurrences of a Substring.py
@@ -1,5 +1,5 @@
 class Solution:
     def removeOccurrences(self, s: str, part: str) -> str:
-        while part in s:  # Keep removing while part exists in s
+        while part in s:                # Keep removing while part exists in s
             s = s.replace(part, "", 1)  # Remove the first occurrence of part
         return s
