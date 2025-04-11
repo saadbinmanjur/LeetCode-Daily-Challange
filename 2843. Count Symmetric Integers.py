935e647209cfe46d2f24c0fe5235c2ed9f313c0e
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-04-11 14:41:04 +0600

    easy

--- 2843. Count Symmetric Integers.py
+++ 2843. Count Symmetric Integers.py
@@ -1,3 +1,17 @@
 class Solution:
     def countSymmetricIntegers(self, low: int, high: int) -> int:
-        
+        def is_symmetric(num: int) -> bool:
+            s = str(num)
+            n = len(s)
+            if n % 2 != 0:
+                return False
+            half = n // 2
+            left_sum = sum(int(d) for d in s[:half])
+            right_sum = sum(int(d) for d in s[half:])
+            return left_sum == right_sum
+
+        count = 0
+        for num in range(low, high + 1):
+            if is_symmetric(num):
+                count += 1
+        return count
