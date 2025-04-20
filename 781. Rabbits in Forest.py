465a582da5974032de7b3a84b1a43b654068a3ed
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-04-20 12:07:42 +0600

    medium

--- 781. Rabbits in Forest.py
+++ 781. Rabbits in Forest.py
@@ -1,3 +1,15 @@
+from typing import List
+from collections import Counter
+import math
+
 class Solution:
     def numRabbits(self, answers: List[int]) -> int:
-        
+        count = Counter(answers)
+        total = 0
+
+        for answer, freq in count.items():
+            group_size = answer + 1
+            num_groups = math.ceil(freq / group_size)
+            total += num_groups * group_size
+
+        return total
