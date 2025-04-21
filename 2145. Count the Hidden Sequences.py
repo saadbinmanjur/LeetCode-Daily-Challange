a32b7da760e2133086a6cdf1b7045cac95245210
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-04-21 12:21:20 +0600

    medium

--- 2145. Count the Hidden Sequences.py
+++ 2145. Count the Hidden Sequences.py
@@ -1,3 +1,15 @@
 class Solution:
     def numberOfArrays(self, differences: List[int], lower: int, upper: int) -> int:
+        min_prefix = 0
+        max_prefix = 0
+        curr = 0
         
+        for diff in differences:
+            curr += diff
+            min_prefix = min(min_prefix, curr)
+            max_prefix = max(max_prefix, curr)
+        
+        min_start = lower - min_prefix
+        max_start = upper - max_prefix
+        
+        return max(0, max_start - min_start + 1)
