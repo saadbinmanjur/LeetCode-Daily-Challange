8deeaa04c3071a15e6a26389f48d37000aa6961b
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-04-24 07:07:03 +0600

    medium

--- 2799. Count Complete Subarrays in an Array.py
+++ 2799. Count Complete Subarrays in an Array.py
@@ -1,3 +1,20 @@
+from collections import defaultdict
+
 class Solution:
     def countCompleteSubarrays(self, nums: List[int]) -> int:
-        
+        total_distinct = len(set(nums))
+        n = len(nums)
+        count = 0
+
+        for left in range(n):
+            freq = defaultdict(int)
+            distinct = 0
+            for right in range(left, n):
+                if freq[nums[right]] == 0:
+                    distinct += 1
+                freq[nums[right]] += 1
+
+                if distinct == total_distinct:
+                    count += 1
+
+        return count
