5551bb38d94a2c323921861a4fc6bb61a20b9628
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-05-02 10:28:30 +0600

    medium

--- 838. Push Dominoes.py
+++ 838. Push Dominoes.py
@@ -1,3 +1,38 @@
 class Solution:
     def pushDominoes(self, dominoes: str) -> str:
+        n = len(dominoes)
+        forces = [0] * n
         
+        # Left to right pass
+        force = 0
+        for i in range(n):
+            if dominoes[i] == 'R':
+                force = n
+            elif dominoes[i] == 'L':
+                force = 0
+            else:
+                force = max(force - 1, 0)
+            forces[i] += force
+        
+        # Right to left pass
+        force = 0
+        for i in range(n-1, -1, -1):
+            if dominoes[i] == 'L':
+                force = n
+            elif dominoes[i] == 'R':
+                force = 0
+            else:
+                force = max(force - 1, 0)
+            forces[i] -= force
+        
+        # Build result
+        result = []
+        for f in forces:
+            if f == 0:
+                result.append('.')
+            elif f > 0:
+                result.append('R')
+            else:
+                result.append('L')
+        
+        return ''.join(result)
