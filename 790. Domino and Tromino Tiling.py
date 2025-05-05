06fa8cb09bd272567fef371aa487ea2518234489
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-05-05 11:26:01 +0600

    medium

--- 790. Domino and Tromino Tiling.py
+++ 790. Domino and Tromino Tiling.py
@@ -1,3 +1,10 @@
 class Solution:
     def numTilings(self, n: int) -> int:
+        MOD = 10**9 + 7
+        dp = [0] * (max(n + 1, 4))  # ensure length at least 4 to handle base cases
+        dp[0], dp[1], dp[2] = 1, 1, 2
+
+        for i in range(3, n + 1):
+            dp[i] = (dp[i - 1] + dp[i - 2] + 2 * sum(dp[0:i - 3 + 1])) % MOD
         
+        return dp[n]
