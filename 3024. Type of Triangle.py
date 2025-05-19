8d3791474c05e7d68c8a92310237997f0c7a3160
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-05-19 10:04:49 +0600

    easy

--- 3024. Type of Triangle.py
+++ 3024. Type of Triangle.py
@@ -1,3 +1,12 @@
+from typing import List
+
 class Solution:
     def triangleType(self, nums: List[int]) -> str:
-        
+        a, b, c = nums
+        if a + b <= c or a + c <= b or b + c <= a:
+            return "none"
+        if a == b == c:
+            return "equilateral"
+        if a == b or a == c or b == c:
+            return "isosceles"
+        return "scalene"
