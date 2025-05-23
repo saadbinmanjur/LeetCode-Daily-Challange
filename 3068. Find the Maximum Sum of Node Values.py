ed3b376a60137fb2f3852d093b1aefe95e641cbd
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-05-23 06:39:49 +0600

    hard

--- 3068. Find the Maximum Sum of Node Values.py
+++ 3068. Find the Maximum Sum of Node Values.py
@@ -1,3 +1,6 @@
 class Solution:
     def maximumValueSum(self, nums: List[int], k: int, edges: List[List[int]]) -> int:
-        
+        no, yes = 0, -inf
+        for n in nums:
+            no, yes = max(n + no, (n^k) + yes), max((n^k) + no, n + yes)
+        return no
