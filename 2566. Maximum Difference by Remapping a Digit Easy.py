be70119abd9a1a1199736e8195deb856a235e929
saadbinmanjur <saadbinmanjuradit@gmail.com>
2025-06-14 17:06:45 +0600

    easy

--- 2566. Maximum Difference by Remapping a Digit Easy.py
+++ 2566. Maximum Difference by Remapping a Digit Easy.py
@@ -1,3 +1,19 @@
 class Solution:
     def minMaxDifference(self, num: int) -> int:
-        
+        s = str(num)
+
+        # Maximize: replace first non-9 digit with 9
+        max_val = num
+        for d in set(s):
+            if d != '9':
+                max_candidate = int(s.replace(d, '9'))
+                max_val = max(max_val, max_candidate)
+
+        # Minimize: replace first digit with 0 (unless it's already 0)
+        min_val = num
+        for d in set(s):
+            if d != '0':
+                min_candidate = int(s.replace(d, '0'))
+                min_val = min(min_val, min_candidate)
+
+        return max_val - min_val
